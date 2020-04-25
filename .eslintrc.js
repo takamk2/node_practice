bbe4a63e71f6b498da9bc6ccc96705b003007fc4
takamk2 <y.m.t.m.h.h.s@gmail.com>
2020-04-26 01:22:04 +0900

    Add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    env: {
+        es6: true,
+        browser: true,
+    },
+    extends: [
+        'eslint:recommended',
+    ],
+    rules: {
+        semi: ['error', 'always'],
+        quotes: ['error', 'single'],
+    }
+};
